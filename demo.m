a9c5428825520afb044eb106ab6a89a9d65c8f50
XiaoweiGu <xw.gu@hotmail.com>
2020-11-21 10:00:10 +0000

    Add files via upload

---
+++ demo.m
@@ -0,0 +1,21 @@
+clear all
+clc
+close all
+
+load exampledata
+%% load training data
+Input1.LabeledData=Data_Train; %% labelled training data
+Input1.GroundTruth=Label_Train; %% labels
+Input1.UnlabeledData=Data_Test; %% unlabeled training data
+Input1.LayerNum=3; %% 
+%% run semi-supervised learning
+[Output1]=STHP(Input1,'ssl');
+%% load testing data
+Input2.UnlabeledData=Data_Test; %% unlabeled training data
+Input2.Syst=Output1.Syst; 
+%% run testing
+[Output2]=STHP(Input2,'ta');
+%% calculate the classification accuracy
+ConfusionMatrix=confusionmat(Label_Test,Output2.Labels);
+Ltemp=size(ConfusionMatrix,1);
+Acc2=sum(sum(ConfusionMatrix.*eye(Ltemp)))/sum(sum(ConfusionMatrix))
